3724278afea4de82a68064fd3b2e66d98ba98197
islahatun Nufusi <islahatunnufusi07@gmail.com>
2024-05-29 14:34:58 +0700

    migrate table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('role')->default('user');
             $table->string('position')->nullable();
             $table->string('department')->nullable();
-            $table->string('face_embedding')->nullable();
+            $table->string('face_embedded')->nullable();
             $table->string('image_url')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
